06a7cb391b1475ef13bfb2b78b15f1f1c02d3330
Eray Gökgör <gokgoreray@gmail.com>
2024-10-04 23:20:57 +0300

    update work

--- chapter1/exercise_1_16/main.c
+++ chapter1/exercise_1_16/main.c
@@ -1,39 +1,42 @@
 #include <stdio.h>
 #define MAXLINE 1000
 
-int get_line(char s[], int lim);
+int get_line(char s[], int maxline);
 void copy(char to[], char from[]);
 
 int main(void)
 {
     int len;
-    int max = 0;
+    int max;
     char line[MAXLINE];
     char longest[MAXLINE];
 
+    max = 0;
     while ((len = get_line(line, MAXLINE)) > 0)
+    {
         if (len > max) {
             max = len;
             copy(longest, line);
         }
 
         printf("length: %d\n", len);
+    }
     if (max > 0)
         printf("%s", longest);
     return 0;
 }
 
-int get_line(char s[], int lim)
+int get_line(char line[], int lim)
 {
     int c, i;
 
     for (i=0; i<lim-1 && (c=getchar()) != EOF && c != '\n'; ++i)
-        s[i] = c;
+        line[i] = c;
     if (c == '\n') {
-        s[i] = c;
+        line[i] = c;
         ++i;
     }
-    s[i] = '\0';
+    line[i] = '\0';
     while(c != EOF && c != '\n')
     {
         ++i;
