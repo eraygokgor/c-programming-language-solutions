f126df477bb16da5ad19a589cb0a14e56eaf62cc
Eray Gökgör <gokgoreray@gmail.com>
2024-10-08 18:34:29 +0300

    initial commit

---
+++ chapter2/exexrcise_2_01/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main(void)
+{
+
+  printf("CHAR MIN: %d\n", SCHAR_MIN);
+  printf("CHAR MAX: %d\n", SCHAR_MAX);
+  printf("\n");
+
+
+  printf("INT MIN: %d\n", INT_MIN);
+  printf("INT MAX: %d\n", INT_MAX);
+  printf("\n");
+
+  printf("SHORT MIN: %d\n", SHRT_MIN);
+  printf("SHORT MAX: %d\n", SHRT_MAX);
+  printf("\n");
+
+  return 0;
+}
